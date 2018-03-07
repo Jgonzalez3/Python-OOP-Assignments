33eb5abf98cfae98e2fa2f58cea6df1d680e1fbf
Javier Gonzalez <javig56@gmail.com>
2018-03-07 08:27:26 -0800

    upload Car Python OOP assignment

---
+++ Car.py
@@ -0,0 +1,29 @@
+# pylint: disable=print-statement
+
+class Car(object):
+    def __init__(self, price, speed, fuel, mileage):
+        self.price = price
+        if price > 10000:
+            self.tax = .15
+        elif price <= 10000:
+            self.tax = .12
+        self.speed = speed
+        self.fuel = fuel
+        self.mileage = mileage
+        self.displayall()
+    def displayall(self):
+        print "Price:", self.price
+        print "Speed:", self.speed
+        print "Fuel:", self.fuel
+        print "Mileage:", self.mileage
+        print "Tax:", self.tax
+        return self
+
+car1 = Car(15000, "100mph", "35mpg", "4000")
+car2 = Car(8000, "70mph", "25mpg", "60000")
+car3 = Car(20000, "120mph", "30mpg", "20000")
+car4 = Car(9000, "100mph", "35mpg", "80000")
+car5 = Car(2000, "65mph", "25mpg", "75000")
+car6 = Car(10000, "90mph", "30mpg", "30000")
+
+print car1, car2, car3, car4, car4, car5, car6
